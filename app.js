0235382339f144f882b776b970d5480b2f23feaf
samuelssen <110110334+samuelssen@users.noreply.github.com>
2024-09-24 12:21:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function sortear(){ 
+    let quantidade = parseInt (document.getElementById('quantidade').value);
+    let de = parseInt (document.getElementById('de').value);
+    let ate = parseInt (document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numero; 
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+      }
+    
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+            alert('Tentando obter número inédito');
+        }
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById ('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();    
+
+    if (de >= ate){
+        alert(`O número informado em "Do número" é maior que o "Até o número"! Verifique a informação. `)
+        reiniciar()
+    }
+}
+
+function obterNumeroAleatorio (min, max){ 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = ''; 
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+
+    
+}
+
+function alterarStatusBotao(){ 
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{ 
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
